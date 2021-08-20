8550de3e3bacd2f23046de42b5cbc3b6b6dfa596
coderme777 <85622586+coderme777@users.noreply.github.com>
2021-08-20 10:50:51 +0300

    Update cz.js

--- game cross-zero/cz.js
+++ game cross-zero/cz.js
@@ -8,27 +8,31 @@ count = 0,
 zero = '<svg class="zero"><circle r="45" cx="58" cy="58" stroke="OrangeRed" stroke-width="11" fill="none" stroke-linecap="round"/></svg>',
 cross ='<svg class="cross"><line class="first" x1="15" y1="15" x2="100" y2="100" stroke="gray" stroke-width="11" stroke-linecap="round"/><line class="second" x1="100" y1="15" x2="15" y2="100" stroke="DarkRed" stroke-width="10" stroke-linecap="round"/></svg>';
 
-function getCross(target){
+function getCross(target)
+{
     target.innerHTML = cross;
     target.classList.add('x');
     let soundCross = new Audio('sound/cross.mp3');
     soundCross.play();
     count++;
 }
-function getZero(target){
+function getZero(target)
+{
     target.innerHTML = zero;
     target.classList.add('o');
     let soundZero = new Audio('sound/zero.mp3');
     soundZero.play();
     count++;
 }
-function getInit(e){ //для инициализации игры
+function getInit(e)
+{ //для инициализации игры
     if(!step) getCross(e.target); //e.target передаем объект, на котором сработало событие
     else getZero(e.target);
     step = !step;
     getWin();
 }
-function getNewGame(){
+function getNewGame()
+{
     step = false;
     count = 0;
     res.innerText = 'Новая игра началась';
@@ -38,7 +42,8 @@ function getNewGame(){
     });
     //game.addEventListener('click', init);//если не работает, то дабавить эту строку
 }
-function getWin(){
+function getWin()
+{
     let combination = [
         [0,1,2],
         [3,4,5],
@@ -49,10 +54,12 @@ function getWin(){
         [1,4,7],
         [2,5,8],
     ];
-    for(let i=0; i<combination.length; i++){
+    for(let i=0; i<combination.length; i++)
+    {
         if(fields[combination[i][0]].classList.contains('x') &&
         fields[combination[i][1]].classList.contains('x') &&
-        fields[combination[i][2]].classList.contains('x')){
+        fields[combination[i][2]].classList.contains('x'))
+        {
             setTimeout(()=>{ //нужна задержка, чтобы значек дорисовался
                 fields[combination[i][0]].classList.add('active');
                 fields[combination[i][1]].classList.add('active'); 
@@ -63,7 +70,8 @@ function getWin(){
         }
         else if(fields[combination[i][0]].classList.contains('o') &&
             fields[combination[i][1]].classList.contains('o') &&
-            fields[combination[i][2]].classList.contains('o')){
+            fields[combination[i][2]].classList.contains('o'))
+        {
                 setTimeout(()=>{ //нужна задержка, чтобы значек дорисовался
                     fields[combination[i][0]].classList.add('active');
                     fields[combination[i][1]].classList.add('active'); 
@@ -72,7 +80,8 @@ function getWin(){
                 },1000);
                 game.removeEventListener('click', getInt);
             }
-        else if(count == 9){
+        else if(count == 9)
+        {
             setTimeout(()=>{ //нужна задержка, чтобы значек дорисовался
                 res.innerText = 'Ничья'; 
             },1000);
